6ce6f52c2d5015d800f9ef5b0839a71d80ab8533
Damian Michalak <damian.michalak@appunite.com>
2015-08-31 09:14:21 +0200

    removed buttons

--- app/src/main/java/com/damianmichalak/beacontest/MainActivity.java
+++ app/src/main/java/com/damianmichalak/beacontest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.damianmichalak.beacontest;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
@@ -24,8 +25,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final View start = findViewById(R.id.button);
-        final View stop = findViewById(R.id.stop);
         webView = (WebView) findViewById(R.id.web_view);
 
         beaconManager = BeaconManager.getInstanceForApplication(MainActivity.this);
@@ -37,30 +36,13 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         beaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
+        Log.d(TAG, "App ready");
 //       UUID="e2 c5 6d b5 df fb 48 d2 b0 60 d0 f5 a7 10 96 e0"
 //       MAJOR="00 16"
 //       MINOR="00 08"
 //       POWER="c5"
 
         beaconManager.bind(MainActivity.this);
-
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-//
-//                webView.setVisibility(View.VISIBLE);
-//                webView.loadUrl("http://panel.mobil-ad.pl/malpka/");
-            }
-        });
-
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webView.setVisibility(View.GONE);
-                webView.loadUrl("about:blank");
-            }
-        });
 
     }
 
@@ -69,24 +51,41 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         beaconManager.setMonitorNotifier(new MonitorNotifier() {
             @Override
             public void didEnterRegion(Region region) {
-                Log.i(TAG, "I just saw an beacon for the first time!");
-
-//                webView.setVisibility(View.VISIBLE);
+                Log.d(TAG, "I just saw an beacon for the first time!");
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        webView.loadUrl("http://panel.mobil-ad.pl/malpka/");
+                    }
+                });
             }
 
             @Override
             public void didExitRegion(Region region) {
-                Log.i(TAG, "I no longer see an beacon");
+                Log.d(TAG, "I no longer see an beacon");
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        webView.loadUrl("about:blank");
+                    }
+                });
             }
 
             @Override
             public void didDetermineStateForRegion(int state, Region region) {
-                Log.i(TAG, "I have just switched from seeing/not seeing beacons: " + state);
+                Log.d(TAG, "I have just switched from seeing/not seeing beacons: " + state);
             }
         });
 
         try {
-            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+            beaconManager.startMonitoringBeaconsInRegion(
+                    new Region("e2 c5 6d b5 df fb 48 d2 b0 60 d0 f5 a7 10 96 e0", null, null, null));
+        } catch (RemoteException e) {
+        }
+
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(
+                    new Region("e2c56db5dffb48d2b060d0f5a71096e0", null, null, null));
         } catch (RemoteException e) {
         }
     }
